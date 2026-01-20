bbe13cb1b8a44cce8ab82a080eab30ca6fade1fb
matrixmickey <matrixmickey@gmail.com>
2026-01-20 16:07:25 -0500

    feat: listen for user from wrapper: attempt 3

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 export default function Home() {
   useEffect(() => {
     function handleMessage(event: MessageEvent) {
-      console.log("User received from wrapper: ", event.data.payload.email);
+      console.log("User received from wrapper: ", event.data.email);
       window.removeEventListener("message", handleMessage);
     }
     window.addEventListener("message", handleMessage);
